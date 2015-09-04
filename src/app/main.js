270dcd58c0c86c9f92c9aed00113f1896ce56999
Maxim_Milovanov <mma29121983@gmail.com>
2015-09-04 16:40:09 +0100

    configs

--- src/app/main.js
+++ src/app/main.js
@@ -4,11 +4,12 @@
 import angular from 'angular';
 import auth from 'authModule';
 import uiRouter from 'angular-ui-router';
+import bui from 'angular-ui-bootstrap';
 import defaultRoute from'./config.default.route';
 import defaultAuthAction from './auth.default.error';
 /*css*/
-import 'bootstap.css';
+import 'bootstap-css-only';
 
-export default angular.module('handsomeTry', [auth, uiRouter])
+export default angular.module('handsomeTry', [auth, uiRouter, bui])
     .config(defaultRoute)
     .run(defaultAuthAction).name;
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
 var webpack = require('webpack');
 
-var commonsPlugin =
-  new webpack.optimize.CommonsChunkPlugin('common.js');
+var BowerWebpackPlugin = require("bower-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app/main.js',
   output: {
-    filename: 'bundle.js'
+    path: './dist',
+    filename: 'bundle.js',
+    hash: true
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} // inline base64 URLs for <=8k images, direct URLs for the rest
+      {test: /\.js$/, loader: 'babel-loader'},
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}, // inline base64 URLs for <=8k images, direct URLs for the rest
+      {test: /\.(woff|svg|ttf|eot)([\?]?.*)$/, loader: "file-loader?name=[name].[ext]"}
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.css']
+    extensions: ['', '.js', '.json', '.css'],
+    alias: {
+      authModule: './common/auth/auth.module.js'
+    }
   },
-  plugins:[commonsPlugin]
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new BowerWebpackPlugin({
+      modulesDirectories: ['bower_components'],
+      manifestFiles: ['bower.json', '.bower.json'],
+      includes: /.*/,
+      excludes: /.*\.less$/
+    }),
+    new HtmlWebpackPlugin({template: './src/index.html', inject: 'body', filename: 'index.html'})
+    //new webpack.optimize.UglifyJsPlugin()
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    inline: true
+  }
 };
